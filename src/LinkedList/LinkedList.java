af758dd38a17d066ba84104bc471da16cad73ffe
Siddharth-IITH <sidg436@gmail.com>
2020-07-24 20:25:55 +0530

    Deleting LinkedList

---
+++ src/LinkedList/LinkedList.java
@@ -0,0 +1,27 @@
+package LinkedList;
+
+class LinkedList {
+
+    Node head;
+
+    class Node {
+
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+
+    }
+
+    void deletelist() {
+        head = null;
+    }
+
+    public void push(int new_data) {
+//            
+    }
+
+}
