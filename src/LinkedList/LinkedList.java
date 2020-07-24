79b437e04952204cd3b33e566454c519125800da
Siddharth-IITH <sidg436@gmail.com>
2020-07-24 22:59:26 +0530

    search key in ll

--- src/LinkedList/LinkedList.java
+++ src/LinkedList/LinkedList.java
@@ -1,51 +1,74 @@
 package LinkedList;
 
+class Node {
+
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+
+}
+
 class LinkedList {
 
     Node head;
-
-    class Node {
-
-        int data;
-        Node next;
-
-        Node(int data) {
-            this.data = data;
-            this.next = null;
-        }
-
-    }
+    int length = 0;
 
     void deletelist() {
         head = null;
     }
 
     public void push(int new_data) {
-        
-        Node new_node= new Node(new_data);
-        new_node.next=head;
-        head=new_node;
+
+        Node new_node = new Node(new_data);
+        new_node.next = head;
+        head = new_node;
+        length++;
 //            
     }
-    
-    void printlist(){
-        while(head!=null){
-            System.out.println(head.data);
-            head=head.next;
+
+    public void printlist() {
+        Node current = head;
+        while (current != null) {
+            System.out.println(current.data);
+            current = current.next;
         }
     }
-    
-    public static void main(String[] args){
+
+    public boolean search(Node head, int key) {
+        Node current = head;
+
+        while (current != null) {
+            if (current.data == key) {
+                return true;
+            }
+            current = current.next;
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
         LinkedList ll = new LinkedList();
         ll.push(1);
         ll.push(2);
         ll.push(3);
         ll.push(4);
-        ll.push(5);
-        
+        ll.push(7);
+
         System.out.println("start printing list: \n");
         ll.printlist();
         System.out.println("\nprinting end.\n");
+
+        System.out.println("Length of LinkedList is: " + ll.length + "\n\n");
+
+        int search_val = 1;
+
+        System.out.println("Search for key=" + search_val + " in LinkedList is: " + String.valueOf(ll.search(ll.head, search_val)) + "\n\n");
+
     }
 
 }
