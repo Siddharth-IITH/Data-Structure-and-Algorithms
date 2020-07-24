d59e67dac2bd82c3106e7e435321f58897240c96
Siddharth-IITH <sidg436@gmail.com>
2020-07-24 22:22:21 +0530

    Print and push methods implementation

--- src/LinkedList/LinkedList.java
+++ src/LinkedList/LinkedList.java
@@ -21,7 +21,31 @@ class LinkedList {
     }
 
     public void push(int new_data) {
+        
+        Node new_node= new Node(new_data);
+        new_node.next=head;
+        head=new_node;
 //            
+    }
+    
+    void printlist(){
+        while(head!=null){
+            System.out.println(head.data);
+            head=head.next;
+        }
+    }
+    
+    public static void main(String[] args){
+        LinkedList ll = new LinkedList();
+        ll.push(1);
+        ll.push(2);
+        ll.push(3);
+        ll.push(4);
+        ll.push(5);
+        
+        System.out.println("start printing list: \n");
+        ll.printlist();
+        System.out.println("\nprinting end.\n");
     }
 
 }
